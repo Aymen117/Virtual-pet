79b78e845791cd583cfb50f0fc0d3cbc9c587909
Aymen117 <65072335+Aymen117@users.noreply.github.com>
2020-10-24 21:08:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var dog,happydog;
+var food,foodstock;
+var database;
+
+function preload()
+{
+  loadImage('images/dogImg.png');
+  loadImage('images/dogImg1.png');
+}
+
+function setup() {
+	createCanvas(500,500);
+  dog=createSprite(100,100,100,100);
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
